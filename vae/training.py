Added training with pytorch lightning, inference, fixed missing cuda tensor movements, fixed sign of the loss

vae/training.py
@@ -2,41 +2,52 @@ from vae.utils.model import VariationalAutoEncoder
 import torch
 from transformers import Trainer, TrainingArguments
 from datasets import load_dataset
+from torchvision.transforms import Compose, RandomRotation, RandomHorizontalFlip, Resize, ColorJitter
+from torchvision.transforms.functional import pil_to_tensor
+from transformers import PretrainedConfig, PreTrainedModel
+import lightning as pl
+from lightning.pytorch.loggers import TensorBoardLogger
 
-# If the dataset is gated/private, make sure you have run huggingface-cli login
-dataset = load_dataset("imagenet-1k")
-train_dataset = dataset["train"]
-valid_dataset = dataset["validation"]
 
+def img_processor(batch):
 
-class VAEWrapper(torch.nn.Module):
+    batch['x'] = [pil_to_tensor(transform(img)) for img in batch['image']]
+    return batch
+
+def collate_fn(images):
+    x = torch.stack([img['x'] for img in images]).float()
+    return x
+
+class VAEWrapper(pl.LightningModule):
     def __init__(self, vae):
+        super().__init__()
+        self.model = vae
 
-        super.__init__()
-        self.vae = vae
+    def forward(self, batch):
+        return batch
 
-    def forward(self, inputs):
+    def training_step(self, batch, batch_idx):
+        return self.model.compute_loss(batch)
 
-        x = inputs["x"]
-        loss = self.vae.compute_loss(x)
-
-        return loss
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(lr=3e-4, params=self.model.parameters())
+    
+dataset = load_dataset("poloclub/diffusiondb", '2m_random_50k')
+batch_size = 32
+transform = Compose([Resize((256,256)), RandomHorizontalFlip(), RandomRotation(20), ColorJitter(brightness=0.25, contrast=0.25, saturation=0.25)])
+train_val = dataset['train'].train_test_split(test_size=0.1, shuffle=True)
+train_dataset = train_val['train']
+val_dataset = train_val['test']
+train_dataset.set_transform(img_processor)
+val_dataset.set_transform(img_processor)
+train_dataloader = torch.utils.data.DataLoader(train_dataset, collate_fn=collate_fn, batch_size=batch_size)
+val_dataloader = torch.utils.data.DataLoader(val_dataset, collate_fn=collate_fn, batch_size=batch_size)
 
 
 vae = VariationalAutoEncoder(encoder_decoder_depth=3, encoder_start_channels=64)
 model = VAEWrapper(vae=vae)
-optimizer = torch.optim.Adam(lr=3e-4, params=model.parameters())
-lr_scheduler = torch.optim.lr_scheduler.ConstantLR(optimizer=optimizer, factor=1.0, last_epoch=-1)
-args = TrainingArguments(
-    output_dir="/Users/leoperelli/Documents/AI Stuff/VAE/trainings",
-    per_device_train_batch_size=16,
-    save_strategy="epoch",
-    save_total_limit=5,
-)
 
-# trainer = Trainer(
-#     model=model,
-#     optimizers=(optimizer, lr_scheduler)
-#     train_dataset=
-#     eval_dataset=
-# )
+# default logger used by trainer (if tensorboard is installed)
+logger = TensorBoardLogger(save_dir='/home/ec2-user/vae/trainings')
+trainer = pl.Trainer(accelerator="gpu", default_root_dir='/home/ec2-user/vae/trainings', max_epochs=20, logger=logger)
+trainer.fit(model=model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)

vae/utils/model.py
@@ -186,9 +186,9 @@ class VariationalAutoEncoder(nn.Module):
             input=self.encoder_sampler.sample_isotropic()[None],
             dim=1,
             sizes=(int(self.latent_dim**0.5), -1),
-        )[:, None, :, :]
+        )[:, None, :, :].to(next(self.parameters()).device)
         log_mu, log_sigma = self.decoder(z)
-        flattened_x = self.decoder_sampler.sample(log_mu, log_sigma)
+        flattened_x = self.decoder_sampler.sample(log_mu, log_sigma).to(next(self.parameters()).device)
         x = torch.unflatten(flattened_x, dim=2, sizes=(int(self.pixel_dim**0.5), -1))
 
         return x
@@ -198,7 +198,7 @@ class VariationalAutoEncoder(nn.Module):
         log_mu_z, log_sigma_z = self.encoder(x)
         sigma_z = torch.exp(log_sigma_z)
         mu_z = torch.exp(log_mu_z)
-        estimated_reconstruction_loss = self.estimate_reconstruction_loss(
+        estimated_reconstruction_likelihood = self.estimate_reconstruction_likelihood(
             x, log_mu_z, log_sigma_z, n_samples=n_samples
         )
         kl_divergence = (
@@ -207,9 +207,9 @@ class VariationalAutoEncoder(nn.Module):
             0
         ]  # B x 1 x 1024 -> B
 
-        return (estimated_reconstruction_loss + kl_divergence).mean()
+        return - (estimated_reconstruction_likelihood + kl_divergence).mean()
 
-    def estimate_reconstruction_loss(self, x, log_mu_z, log_sigma_z, n_samples=1):
+    def estimate_reconstruction_likelihood(self, x, log_mu_z, log_sigma_z, n_samples=1):
 
         log_probs = []
         for _ in range(n_samples):

vae/utils/samplers.py
@@ -12,6 +12,7 @@ class MultivariateNormal:
     def sample(self, log_mu, log_sigma):
 
         samples = self.normal.sample((log_mu.shape[0:2]))
+        samples = samples.to(log_mu.device)
 
         return torch.exp(log_mu) + torch.exp(log_sigma) * samples[0]
 
@@ -23,7 +24,7 @@ class MultivariateNormal:
 
         original_shape = log_mu.shape
         mu = torch.exp(log_mu)
-        sigma_square = torch.pow(torch.exp(log_sigma), 2)
+        sigma_square = torch.pow(torch.exp(log_sigma), 2) + 0.001 * torch.ones_like(log_sigma)
 
         mu = mu.flatten()
         sigma_square = sigma_square.flatten()