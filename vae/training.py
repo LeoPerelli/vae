Bug fix in kl divergence and log likelihood

vae/training.py
@@ -1,16 +1,13 @@
-from vae.utils.model import VariationalAutoEncoder
+from vae.utils.model import VariationalAutoEncoder, SimpleVariationalAutoEncoder
 import torch
-from transformers import Trainer, TrainingArguments
 from datasets import load_dataset
 from torchvision.transforms import Compose, RandomRotation, RandomHorizontalFlip, Resize, ColorJitter
 from torchvision.transforms.functional import pil_to_tensor
-from transformers import PretrainedConfig, PreTrainedModel
 import lightning as pl
 from lightning.pytorch.loggers import TensorBoardLogger
 
 
 def img_processor(batch):
-
     batch['x'] = [pil_to_tensor(transform(img)) for img in batch['image']]
     return batch
 
@@ -27,27 +24,54 @@ class VAEWrapper(pl.LightningModule):
         return batch
 
     def training_step(self, batch, batch_idx):
-        return self.model.compute_loss(batch)
+        estimated_reconstruction_likelihood, kl_divergence = self.model.compute_loss(batch)
+        self.log("kl_divergence", kl_divergence.mean(), on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log("estimated_reconstruction_likelihood", estimated_reconstruction_likelihood.mean(), on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        
+        loss = (kl_divergence - estimated_reconstruction_likelihood).mean()
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+    # def validation_step(self, batch, batch_idx):
+    #     estimated_reconstruction_likelihood, kl_divergence = self.model.compute_loss(batch)
+    #     self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+    #     return loss
+
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(lr=3e-4, params=self.model.parameters())
     
-dataset = load_dataset("poloclub/diffusiondb", '2m_random_50k')
+# dataset = load_dataset("poloclub/diffusiondb", '2m_random_50k')
+# batch_size = 32
+# img_dim = 32
+# transform = Compose([Resize((img_dim,img_dim)), RandomHorizontalFlip(), RandomRotation(20), ColorJitter(brightness=0.25, contrast=0.25, saturation=0.25)])
+# train_val = dataset['train'].train_test_split(test_size=0.1, shuffle=True)
+# train_dataset = train_val['train']
+# val_dataset = train_val['test']
+# train_dataset.set_transform(img_processor)
+# val_dataset.set_transform(img_processor)
+# train_dataloader = torch.utils.data.DataLoader(train_dataset, collate_fn=collate_fn, batch_size=batch_size)
+# val_dataloader = torch.utils.data.DataLoader(val_dataset, collate_fn=collate_fn, batch_size=batch_size)
+
+dataset = load_dataset("mnist")
 batch_size = 32
-transform = Compose([Resize((256,256)), RandomHorizontalFlip(), RandomRotation(20), ColorJitter(brightness=0.25, contrast=0.25, saturation=0.25)])
-train_val = dataset['train'].train_test_split(test_size=0.1, shuffle=True)
-train_dataset = train_val['train']
-val_dataset = train_val['test']
-train_dataset.set_transform(img_processor)
-val_dataset.set_transform(img_processor)
-train_dataloader = torch.utils.data.DataLoader(train_dataset, collate_fn=collate_fn, batch_size=batch_size)
-val_dataloader = torch.utils.data.DataLoader(val_dataset, collate_fn=collate_fn, batch_size=batch_size)
+img_dim = 28
+train_dataset = dataset['train']
+val_dataset = dataset['test']
+transform = Compose([Resize((img_dim,img_dim))])
+def img_processor_2(batch):
 
+    batch['x'] = [pil_to_tensor(transform(img)).flatten(start_dim=1) for img in batch['image']]
+    return batch
+train_dataset.set_transform(img_processor_2)
+val_dataset.set_transform(img_processor_2)
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, collate_fn=collate_fn)
+val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, collate_fn=collate_fn)
 
-vae = VariationalAutoEncoder(encoder_decoder_depth=3, encoder_start_channels=64)
+vae = SimpleVariationalAutoEncoder()
 model = VAEWrapper(vae=vae)
 
 # default logger used by trainer (if tensorboard is installed)
 logger = TensorBoardLogger(save_dir='/home/ec2-user/vae/trainings')
-trainer = pl.Trainer(accelerator="gpu", default_root_dir='/home/ec2-user/vae/trainings', max_epochs=20, logger=logger)
+trainer = pl.Trainer(accelerator="gpu", default_root_dir='/home/ec2-user/vae/trainings', overfit_batches=1,max_epochs=200, logger=logger, log_every_n_steps=50)
 trainer.fit(model=model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)

vae/utils/model.py
@@ -166,7 +166,7 @@ class ResidualDecoder(nn.Module):
 
 
 class VariationalAutoEncoder(nn.Module):
-    def __init__(self, encoder_decoder_depth, encoder_start_channels):
+    def __init__(self, encoder_decoder_depth, encoder_start_channels, img_dim = 256):
 
         super().__init__()
 
@@ -175,8 +175,8 @@ class VariationalAutoEncoder(nn.Module):
             in_channels_start=int(encoder_start_channels * (2) ** encoder_decoder_depth),
             depth=encoder_decoder_depth,
         )
-        self.latent_dim = 32 * 32
-        self.pixel_dim = 256 * 256
+        self.latent_dim = int(img_dim * 0.5** encoder_decoder_depth) ** 2
+        self.pixel_dim = img_dim * img_dim
         self.encoder_sampler = MultivariateNormal(dimension=self.latent_dim)
         self.decoder_sampler = MultivariateNormal(dimension=self.pixel_dim)
 
@@ -201,13 +201,13 @@ class VariationalAutoEncoder(nn.Module):
         estimated_reconstruction_likelihood = self.estimate_reconstruction_likelihood(
             x, log_mu_z, log_sigma_z, n_samples=n_samples
         )
-        kl_divergence = (
+        kl_divergence = -(
             0.5 * (torch.ones_like(log_mu_z) + 2 * log_mu_z - torch.pow(mu_z, 2) - torch.pow(sigma_z, 2))
         ).sum(dim=2)[
             0
         ]  # B x 1 x 1024 -> B
 
-        return - (estimated_reconstruction_likelihood + kl_divergence).mean()
+        return estimated_reconstruction_likelihood, kl_divergence
 
     def estimate_reconstruction_likelihood(self, x, log_mu_z, log_sigma_z, n_samples=1):
 
@@ -231,8 +231,101 @@ class VariationalAutoEncoder(nn.Module):
         return probs
 
 
-# vae = VariationalAutoEncoder(encoder_decoder_depth=3, encoder_start_channels=64)
+
+class SimpleVariationalAutoEncoder(nn.Module):
+    def __init__(self,):
+
+        super().__init__()
+
+        self.latent_dim = 200
+        self.hidden_dim = 400
+        self.pixel_dim = 784
+
+        # encoder
+        self.FC_input = nn.Linear(self.pixel_dim, self.hidden_dim)
+        self.FC_input2 = nn.Linear(self.hidden_dim, self.hidden_dim)
+        self.FC_mean  = nn.Linear(self.hidden_dim, self.latent_dim)
+        self.FC_var   = nn.Linear (self.hidden_dim, self.latent_dim)
+
+        self.relu = nn.ReLU()
+
+        # decoder
+        self.FC_hidden = nn.Linear(self.latent_dim, self.hidden_dim)
+        self.FC_hidden2 = nn.Linear(self.hidden_dim, self.hidden_dim)
+        self.FC_output_mean = nn.Linear(self.hidden_dim, self.pixel_dim)
+        self.FC_output_var = nn.Linear(self.hidden_dim, self.pixel_dim)
+
+        self.encoder_sampler = MultivariateNormal(dimension=self.latent_dim)
+        self.decoder_sampler = MultivariateNormal(dimension=self.pixel_dim)
+
+    def encoder(self,x):
+        h_       = self.relu(self.FC_input(x))
+        h_       = self.relu(self.FC_input2(h_))
+        log_mean     = self.FC_mean(h_)
+        log_var  = self.FC_var(h_)                                                
+        return log_mean, log_var
+
+    def decoder(self, z):
+        h     = self.relu(self.FC_hidden(z))
+        h     = self.relu(self.FC_hidden2(h))
+        log_mean = self.FC_output_mean(h)
+        log_var = self.FC_output_var(h)
+        return log_mean, log_var
+
+
+    def generate(self):
+
+        z = torch.unflatten(
+            input=self.encoder_sampler.sample_isotropic()[None],
+            dim=1,
+            sizes=(int(self.latent_dim**0.5), -1),
+        )[:, None, :, :].to(next(self.parameters()).device)
+        log_mu, log_sigma = self.decoder(z)
+        flattened_x = self.decoder_sampler.sample(log_mu, log_sigma).to(next(self.parameters()).device)
+        x = torch.unflatten(flattened_x, dim=2, sizes=(int(self.pixel_dim**0.5), -1))
+
+        return x
+
+    def compute_loss(self, x, n_samples=1):
+
+        log_mu_z, log_sigma_z = self.encoder(x)
+        sigma_z = torch.exp(log_sigma_z)
+        mu_z = torch.exp(log_mu_z)
+        estimated_reconstruction_likelihood = self.estimate_reconstruction_likelihood(
+            x, log_mu_z, log_sigma_z, n_samples=n_samples
+        )
+        kl_divergence = -(
+            0.5 * (torch.ones_like(log_mu_z) + 2 * log_sigma_z - torch.pow(mu_z, 2) - torch.pow(sigma_z, 2))
+        ).sum(dim=2)[0]  # B x 1 x 1024 -> B
+
+        return estimated_reconstruction_likelihood, kl_divergence
+
+    def estimate_reconstruction_likelihood(self, x, log_mu_z, log_sigma_z, n_samples=1):
+
+        log_probs = []
+        for _ in range(n_samples):
+            z = self.encoder_sampler.sample(log_mu=log_mu_z, log_sigma=log_sigma_z)
+            log_mu_x, log_sigma_x = self.decoder(z)
+
+            log_probs.append(
+                self.decoder_sampler.log_prob(
+                    log_mu=log_mu_x, log_sigma=log_sigma_x, x=x
+                )
+            )  # B x 3 x pixel_dim
+
+        probs = torch.stack(log_probs, dim=1)  # B x n_samples x 3 x pixel_dim
+        probs = probs.sum(dim=[2, 3]).mean(dim=1)  # B
+        return probs
+
+
+
+# vae = SimpleVariationalAutoEncoder()
+# x = torch.ones((2,1,784))
+# y = vae.compute_loss(x=x)
+
+
+# vae = VariationalAutoEncoder(encoder_decoder_depth=3, encoder_start_channels=32)
 # x = torch.ones((2, 3, 256, 256))
 # y = vae.compute_loss(x=x)
 # print(y)
-# add tests for everything
+

vae/utils/samplers.py
@@ -24,10 +24,10 @@ class MultivariateNormal:
 
         original_shape = log_mu.shape
         mu = torch.exp(log_mu)
-        sigma_square = torch.pow(torch.exp(log_sigma), 2) + 0.001 * torch.ones_like(log_sigma)
+        sigma = torch.exp(log_sigma) + 0.001 * torch.ones_like(log_sigma)
 
         mu = mu.flatten()
-        sigma_square = sigma_square.flatten()
+        sigma = sigma.flatten()
 
-        gaussian = torch.distributions.normal.Normal(loc=mu, scale=sigma_square)
+        gaussian = torch.distributions.normal.Normal(loc=mu, scale=sigma)
         return gaussian.log_prob(x.flatten()).unflatten(dim=0, sizes=original_shape)